38661775e5451f14d87043476034cfe982343e54
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-27 21:57:15 +0530

    Adding routing and setting up a resources functional component

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return(
+    <div>
+      Arsenal Football Club
+        <ul>
+          <li>Amazing Club with History</li>
+          <li>London's finest</li>
+          <li>Arsene Wenger built the club</li>
+        </ul>
+    </div>
+  );
+}
